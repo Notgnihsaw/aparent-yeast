5aa163247818894de712afb8fb66e0ab4af1fc7c
lab <lab@stu-217-9.stu.vpn.colby.edu>
2019-07-12 10:14:26 -0400

    Finalized data_retrieval directory

--- data_retrieve/retrieve_seqeunces.py
+++ data_retrieve/retrieve_seqeunces.py
@@ -8,12 +8,24 @@ Created on Thu Jul 11 11:20:19 2019
 
 import location_to_query 
 import seq_fetch
-import to_df 
 
 import pandas as pd
+import csv
 
-def read_and_query(filename, padding, return_type = 'df'):
-    count_dataframe = to_df.to_dataframe(filename)
+def to_dict(filepath):
+    tsv = open(filepath, 'r')
+    
+    loaded = list(zip(*(line.strip().split('\t') for line in tsv)))
+
+    count_dict = {'Location':loaded[0], 'tag_count':loaded[1], 'distinct_tags':loaded[2], 'degeneracy':loaded[3]}
+
+    tsv.close()
+    
+    return count_dict
+
+
+def read_and_query(filename, padding, return_type = 'dict'):
+    site_count_dict = to_dict(filename)
     
     query_file = open(location_to_query.to_query_file(filename, padding), 'r')
     
@@ -22,23 +34,28 @@ def read_and_query(filename, padding, return_type = 'df'):
     for query in query_file:
         seq_list.append(seq_fetch.run('saccharomyces_cerevisiae', query))
     
-    print(seq_list)
-    print(str(len(count_dataframe)) + '\n' + str(len(seq_list)))
-        
-    #count_dataframe = count_dataframe.append(seq_list)
+    #print(seq_list)
+    #print(str(len(site_count_dict)) + '\n' + str(len(seq_list)))
     
-    sequence_dataframe = pd.DataFrame(count_dataframe)
-    
-    if return_type == 'df':
-        return count_dataframe
+    site_count_dict['sequence'] = seq_list
+
+    if return_type == 'dict':
+        return site_count_dict
+    elif return_type == 'df':
+        return pd.DataFrame(site_count_dict)
     elif return_type == 'seqs':
         return seq_list
     else:
-        print("return_type invalid, please specify either df (dataframe) or seqs (sequences).")
+        print("return_type invalid, please specify either dict (dictionary) or seqs (sequences).")
+        
+'''def write_to_csv(input_dict, csv_name, columns):
+    with open(csv_name, 'w+') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(columns)
+        for data in input_dict:
+            writer.writerow(data)'''
     
 
 yeast_df = read_and_query("SiteCounts/site_count_test.txt", 10, 'df')
 
-
-yeast_df.to_csv('yeast_df_site_count', sep='    ')
-
+yeast_df.to_csv('SiteCounts/site_counts_with_sequence.csv')
